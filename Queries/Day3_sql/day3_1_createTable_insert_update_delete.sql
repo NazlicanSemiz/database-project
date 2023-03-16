94afd092ab9822c491b810a2b2b740eeba8d4323
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-03-16 14:04:12 +0000

    day3 DDL(CREATE,DROP,TRUNCATE,ALTER) & DML(SELECT,INSERT,UPDATE,DELETE) Create Table , Insert, Update, Delete

--- Queries/Day3_sql/day3_1_createTable_insert_update_delete.sql
+++ Queries/Day3_sql/day3_1_createTable_insert_update_delete.sql
@@ -8,7 +8,9 @@ create table syntax:
     );
 */
 
---
+--varchar is like String & (30) is limitation of characters
+--primary key: you should put notNull and unique data.
+
 CREATE TABLE ScrumTeam(
   Emp_ID INTEGER PRIMARY KEY,
   FirstName varchar(30) not null,
@@ -18,3 +20,58 @@ CREATE TABLE ScrumTeam(
 );
 
 select * from SCRUMTEAM;
+DROP TABLE SCRUMTEAM;
+
+/*
+ INSERT INTO tableName (column1, column2,...)
+ VALUES (value1, value2,..);
+ */
+
+ INSERT INTO SCRUMTEAM(EMP_ID, FIRSTNAME, LASTNAME, JOBTITLE)
+VALUES (1,'Nazli','Semiz','Tester');
+
+--or
+
+INSERT INTO SCRUMTEAM VALUES (2,'Cilve','Semiz','Developer');
+
+INSERT INTO SCRUMTEAM VALUES (3,'Severus','Snape','ScrumMaster');
+
+INSERT INTO SCRUMTEAM VALUES (4,'Harry','Buffay','PO');
+
+select * from SCRUMTEAM;
+
+
+
+--how to update data
+/*
+UPDATE table_name
+SET column1 = value1,
+column2 = value2 , ...
+WHERE condition;
+*/
+
+-- harry will be tester
+UPDATE SCRUMTEAM
+SET JOBTITLE = 'Tester'
+WHERE EMP_ID= 4;
+
+select *from SCRUMTEAM;
+
+
+
+
+--Delete from table
+/*
+DELETE FROM table_name
+WHERE condition;
+*/
+
+DELETE FROM SCRUMTEAM
+WHERE EMP_ID= 3;
+
+select *from SCRUMTEAM;
+
+
+--saving changes
+commit;
+--when you change sth from tables database, you should commit
